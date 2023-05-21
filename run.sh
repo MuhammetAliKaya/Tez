befb4c09d96f8c197cbde0bfe04f73cbc4606943
unknown <malikayagenetik@gmail.com>
2023-05-22 01:21:08 +0300

    force update

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 cd /app  
 composer install
-php artisan migrate:fresh 
-php artisan db:seed --class=temp_seed
+php artisan migrate:fresh --force
+php artisan db:seed --class=temp_seed --force
 php artisan serve --host=0.0.0.0 --port=8000
